8ab3562c9119ea409e6a2fb00890774d50ef5773
maxiunan78 <742655625@qq.com>
2021-10-29 17:08:52 +0800

    dd

--- utils/handle_encode.py
+++ utils/handle_encode.py
@@ -12,7 +12,7 @@ def sortKeys(param):
 def md5_key(arg):
     hash = hashlib.md5()
     hash.update(arg.encode(encoding='utf-8'))
-    return hash.hexdigest()
+    return hash.hexdigest().upper()
 
 def get_signStr(sortedData,app_key):
     result = ''
@@ -23,16 +23,7 @@ def get_signStr(sortedData,app_key):
     print(sinStr)
     return sinStr
 
-
-if  __name__ == '__main__':
-    appId = '17442'
-    app_key = '86079af0-ef2b-4b55-b8a0-8d29f4e15379'
-    rowData = {'appId':appId,
-               'app_key':app_key,
-               'timestamp':'2016-01-01 12:00:00',
-               'signMethod': 'md5',
-               'memberId':'12123'
-               }
-    sortedData = sortKeys(rowData)
-    app_sign = md5_key(get_signStr(sortedData,app_key))
-    print(app_sign)
+#
+# if  __name__ == '__main__':
+#
+#     print(app_sign)
